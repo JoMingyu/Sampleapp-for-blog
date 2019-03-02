b1fd08fa28f6ffa98f4566f9bcb1af43ac9a880a
JoMingyu <mingyu.planb@gmail.com>
2019-03-03 02:17:10 +0900

    [#6] DB connection 관련 코드 추가

--- config/db_config.py
+++ config/db_config.py
@@ -1,6 +1,19 @@
+import os
+
+
+DB_NAME = 'stay'
+
+
 class LocalDBConfig:
-    pass
+    MAIN_DB_URL = 'mysql+mysqlclient://stay_local:b,to#98gks1VK0@127.0.0.1:3306/{}?charset=utf8mb4'.format(
+        DB_NAME
+    )
 
 
 class RemoteDBConfig:
-    pass
+    MAIN_DB_URL = 'mysql+mysqlclient://{}:{}@{}:3306/{}?charset=utf8mb4'.format(
+        os.getenv('DB_USER'),
+        os.getenv('DB_PASSWORD'),
+        os.getenv('DB_ENDPOINT'),
+        DB_NAME
+    )
