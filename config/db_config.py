88ab8b7b469d3a9649011acf8fde041debda84f8
JoMingyu <mingyu.planb@gmail.com>
2019-03-04 21:13:43 +0900

    [#13] mysql DBAPI library를 mysqlclient에서 mysqldb로 변경

--- config/db_config.py
+++ config/db_config.py
@@ -5,13 +5,13 @@ DB_NAME = 'stay'
 
 
 class LocalDBConfig:
-    MAIN_DB_URL = 'mysql+mysqlclient://stay_local:b,to#98gks1VK0@127.0.0.1:3306/{}?charset=utf8mb4'.format(
+    MAIN_DB_URL = 'mysql+mysqldb://stay_local:b,to#98gks1VK0@127.0.0.1:3306/{}?charset=utf8mb4'.format(
         DB_NAME
     )
 
 
 class RemoteDBConfig:
-    MAIN_DB_URL = 'mysql+mysqlclient://{}:{}@{}:3306/{}?charset=utf8mb4'.format(
+    MAIN_DB_URL = 'mysql+mysqldb://{}:{}@{}:3306/{}?charset=utf8mb4'.format(
         os.getenv('DB_USER'),
         os.getenv('DB_PASSWORD'),
         os.getenv('DB_ENDPOINT'),
